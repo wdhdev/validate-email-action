aaefb282666a504e0cfb4c3eb9159a49c941e036
William Harrison <william@williamdavidharrison.com.au>
2023-05-27 09:23:37 +0800

    feat: use async func

--- index.js
+++ index.js
@@ -5,34 +5,38 @@ const dns = require("dns");
 const util = require("util");
 const validateEmail = require("@williamharrison/validate-email");
 
-try {
-    const email = core.getInput("email");
-    const domain = email.split("@").pop();
+async function action() {
+    try {
+        const email = core.getInput("email");
+        const domain = email.split("@").pop();
 
-    const validEmail = validateEmail(email);
+        const validEmail = validateEmail(email);
 
-    if(!validEmail) return core.setFailed("The email address does not match the correct format!");
+        if(!validEmail) return core.setFailed("The email address does not match the correct format!");
 
-    const getMXRecords = util.promisify(dns.resolveMx);
-    const mxRecords = getMXRecords(domain);
+        const getMXRecords = util.promisify(dns.resolveMx);
+        const mxRecords = await getMXRecords(domain);
 
-    if(!mxRecords.length) return core.setFailed(`No MX records exist for the domain ${domain}!`);
+        if(!mxRecords.length) return core.setFailed(`No MX records exist for the domain ${domain}!`);
 
-    const result = {
-        "success": true,
-        "email": email,
-        "test_results": {
-            "matches_format": true,
-            "mx_exists": true
-        },
-        "results": {
-            "domain": domain,
-            "mx_records": mxRecords
+        const result = {
+            "success": true,
+            "email": email,
+            "test_results": {
+                "matches_format": true,
+                "mx_exists": true
+            },
+            "results": {
+                "domain": domain,
+                "mx_records": mxRecords
+            }
         }
-    }
 
-    console.log(result);
-    core.setOutput("result", result);
-} catch(err) {
-    core.setFailed(err.message);
+        console.log(result);
+        core.setOutput("result", result);
+    } catch(err) {
+        core.setFailed(err.message);
+    }
 }
+
+action()
